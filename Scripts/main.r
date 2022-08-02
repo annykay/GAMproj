b3b5a1f9d23c2030a483696a616f595f4cd1e960
Anna Kondrashina <annykay@mail.ru>
2022-08-02 17:43:36 +0300

    Changes in dataset

--- Scripts/main.r
+++ Scripts/main.r
@@ -7,7 +7,7 @@ data <- dfFactorize(data, c('SMKSTAT', 'WHOSTATN',  'EGFRMUTN'))
 data <- dfOut(data)
 
 data1 <- dfNormalize(data)
-markers <- sort(unique(data1$YTYPE_DESC))
+markers <- sort(unique(data1$YTYPE_DESC))[-8]
 data1 <- chooseTransform(data1, 'norm', rep('norm', length(markers)), markers)
 
 data2 <- data1[, c('USUBJID', 'SMKSTAT', 'TIME', 'WHOSTATN',  'EGFRMUTN', 'YTYPE_DESC', 'target', 'exclude')]
@@ -22,12 +22,17 @@ for (i in c(1:rows)) {
         data2$SLD[data2$USUBJID == subj & abs(data2$TIME - time) < 4 & is.na(data2$SLD)] <- data2$SLD[i]
     }
 }
-data2$exclude[i] <- 3
+
+print(sum(is.na(data2$AST) & is.na(data2$CREAT) & is.na(data2$NLR) & is.na(data2$LDH)))
+data2$exclude[is.na(data2$AST) & is.na(data2$CREAT) & is.na(data2$NLR) & is.na(data2$LDH)] <- 3
 
 set.seed(42)
-res <- stratified(data2, c('SMKSTAT', 'WHOSTATN', 'EGFRMUTN'), 0.7, bothSets= T)
-train <- res$SAMP1
-test <- res$SAMP2
+for_strat <- data2[!(data2$exclude %in% c(1,3)), c('USUBJID', 'SMKSTAT', 'WHOSTATN', 'EGFRMUTN')]
+for_strat <- unique(for_strat)
+res <- stratified(for_strat, c('SMKSTAT', 'WHOSTATN', 'EGFRMUTN'), 0.7, bothSets= T)
+train <- data2[!(data2$exclude %in% c(1,3)) & data2$USUBJID %in% res$SAMP1$USUBJID, ]
+test <- data2[!(data2$exclude %in% c(1,3)) & data2$USUBJID %in% res$SAMP2$USUBJID, ]
+
 
 #ct1 <- gam(SLD~
            #ti(ALT, TIME, bs = 'tp', k = 10) + #
@@ -49,5 +54,5 @@ test <- res$SAMP2
            #s(WBC, bs = 'tp', k = 10) +
            #SMKSTAT,
            #data = train, select=TRUE,
-           #method="REML", family.mgcv = "gauss")
+           #method="REML", family.mgcv = "gaulss")
 #AIC(ct1)
