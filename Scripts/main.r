a697274d1f20f0dfce96967d295c690e2f9d04a7
Alexandr <annykay@mail.ru>
2022-08-03 16:08:01 +0300

    Added statistics calc to main

--- Scripts/main.r
+++ Scripts/main.r
@@ -16,6 +16,12 @@ data1 <- dfNormalize(data)
 markers <- sort(unique(data1$YTYPE_DESC))[-8]
 data1 <- chooseTransform(data1, 'norm', rep('norm', length(markers)), markers)
 
+statistic <- basic_biomarker_statistics(data1[!(data1$exclude %in% c(1, 3)), ], markers)
+write_xlsx(statistic, './DerivedData/MarkerStat.xlsx', format_headers = TRUE)
+
+factor_stat <- factor_stat(c('SMKSTAT', 'WHOSTATN', 'EGFRMUTN'), data1[!(data1$exclude %in% c(1, 3)), ])
+write_xlsx(factor_stat, './DerivedData/FactorStat.xlsx', format_headers = TRUE)
+
 data2 <- data1[, c('USUBJID', 'SMKSTAT', 'TIME', 'WHOSTATN',  'EGFRMUTN', 'YTYPE_DESC', 'target', 'exclude')]
 data2 <- spread(data2, key=YTYPE_DESC, value=target)
 
@@ -31,6 +37,9 @@ for (i in c(1:rows)) {
 
 
 data2$exclude[is.na(data2$AST) & is.na(data2$CREAT) & is.na(data2$NLR) & is.na(data2$LDH)] <- 3
+
+write_xlsx(data1, 'DerivedData/long.xlsx')
+write_xlsx(data2, 'DerivedData/wide.xlsx')
 
 set.seed(42)
 for_strat <- data2[!(data2$exclude %in% c(1,3)), c('USUBJID', 'SMKSTAT', 'WHOSTATN', 'EGFRMUTN')]
